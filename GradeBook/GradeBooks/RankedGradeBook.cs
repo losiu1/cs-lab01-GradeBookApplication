ad4d61ecc342a4662bdccf479f40a892e9adea70
Łukasz Woś <lukasz.wos21@gmail.com>
2022-03-19 17:10:04 +0100

    I have override the RankeedGradeBook and have Providen the appropriate grades based on how the input grade compares to other students

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +10,27 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
+            }
+
+            var  Calc = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[Calc - 1])
+                return 'A';
+            if (averageGrade >= grades[(Calc * 2) - 1])
+                return 'B';
+            if (averageGrade >= grades[(Calc * 3) - 1])
+                return 'C';
+            if (averageGrade >= grades[(Calc * 4) - 1])
+                return 'D';
+            return 'F';
         }
     }
 }
